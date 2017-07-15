21faba7891902b03a112b6c6c780b213af43e49f
muhammad-anwaar <anwaarnuml@gmail.com>
2017-07-15 23:00:38 +0800

    code changes

--- Rename_move.py
+++ Rename_move.py
@@ -1,21 +1,19 @@
 import os
-
-folder='Trainset_W_M_set1'
+#folder name here
+folder=''
 
 for root, dirs, files in os.walk(folder):
     for i,f in enumerate(files):
         absname = os.path.join(root, f)
         f_name, f_ext = os.path.splitext(f)
-        #newname = os.path.join(root, str(i))
+        #check file extension
         if f_ext == '.jpg':   
             name_number, age= f_name.split('M');
-            name_1=age+'_0_0_'+name_number+'M_morph'
+            #renaming file
+            name_1=age+'_0_0_'+name_number+'-'
             name_1=name_1.strip()
             new_name = '{}{}'.format(name_1, f_ext)
             new_name='Male/'+new_name
-            #print absname,'=====> New Name =======>', new_name 
-            #exit(0)
-            #print 'f name===>', f_name , '==ext===>', f_ext
-            #print 'age=', age, '===>number name <=====', name_number
+            #finallly move to another folder
             os.rename(absname, new_name)
         
